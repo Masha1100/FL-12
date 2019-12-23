9827bc82e6b35aed0fd20e8d77b683ee3a92792b
Masha1100 <mashka20d@gmail.com>
2019-12-23 23:36:51 +0200

    add first

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,27 @@
-// Your code goes here
+/* eslint-disable no-useless-catch */
+let a = prompt('Enter number a', '');
+a = parseInt(a, 10);
+if(a === null || isNaN(a)){
+  console.log('Invalid input data');
+}
+
+let b = prompt('Enter number b', '');
+b = parseInt(b, 10);
+if (b === null || isNaN(b)) {
+  console.log('Invalid input data');
+}
+
+let c = prompt('Enter number c', '');
+c = parseInt(c, 10);
+if (c === null || isNaN(c)) {
+  console.log('Invalid input data');
+}
+let discrnum = 4;
+let discriminant = b*b - discrnum*a*c;
+if(discriminant===0){
+  console.log('-1');
+} else if (discriminant < 0){
+  console.log('no solution');
+} else {
+  console.log(discriminant);
+}
