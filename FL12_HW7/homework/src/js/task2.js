ed40760ecf3b3016234db351b410ec900caae08e
Masha1100 <mashka20d@gmail.com>
2019-12-30 23:55:44 +0200

    task two

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let request = confirm('Do you want to play a game?');
+if(request === true) {
+  let question = 'Choose a roulette pocket number from 0 to 8',
+  attempt = 3,
+  prize = 100,
+  total_prize = 0,
+  max = 8,
+  start_num = Math.ceil(0),
+  end_num = Math.floor(max),
+  game_num = Math.floor(Math.random() * (end_num - start_num + 1)) + start_num,
+  left = 'Attempts left:',
+  current = 'Possible prize on current attempt:';
+  let game = prompt(question + '<br>' + left+attempt + '<br>Total prize:' + total_prize + '$<br>' + current+prize);
+    game;
+    if(game === game_num){
+      total_prize +=prize;
+    }
+}else{
+  alert('You did not become a billionaire, but can.')
+}
